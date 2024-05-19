62a48dd1e03e5fdbd1ac4bec4f76d63421374bc5
ntwalii <ntwaliaubin@gmail.com>
2024-05-19 22:52:35 +0200

    Small small changes

--- iterator.js
+++ iterator.js
@@ -1,13 +1,18 @@
 const digits = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+
 const arrayIterator = digits[Symbol.iterator]();
 
-console.log(arrayIterator.next());
-console.log(arrayIterator.next());
-console.log(arrayIterator.next());
-console.log(arrayIterator.next());
-console.log(arrayIterator.next());
-console.log(arrayIterator.next());
-console.log(arrayIterator.next());
-console.log(arrayIterator.next());
-console.log(arrayIterator.next());
-console.log(arrayIterator.next());
+for(const arr of arrayIterator){
+    console.log(arr)
+}
+
+// console.log(arrayIterator.next());
+// console.log(arrayIterator.next());
+// console.log(arrayIterator.next());
+// console.log(arrayIterator.next());
+// console.log(arrayIterator.next());
+// console.log(arrayIterator.next());
+// console.log(arrayIterator.next());
+// console.log(arrayIterator.next());
+// console.log(arrayIterator.next());
+// console.log(arrayIterator.next());
--- let-const.js
+++ let-const.js
@@ -1,9 +1,10 @@
 if(true){
+    console.log(y);
     var x="var"
     let y="let"
     const z="const"
 }
 
 console.log(x);
-// console.log(y); Returns an error
-// console.log(z); Returns an error
+// console.log(y); //Returns an error
+// console.log(z); //Returns an error
