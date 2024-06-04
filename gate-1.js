7793967383c45870939bd52215d3869d1573db26
ntwalii <ntwaliaubin@gmail.com>
2024-06-04 18:26:05 +0200

    New gate questions

---
+++ gate-1.js
@@ -0,0 +1,16 @@
+const input = ["Hello123", "World456", "49", "Another789"];
+
+
+// console.log(output);
+output: [  "Hello",123, "World", 456, 49, "Another", 789] // Expected output
+
+
+let extractWordsAndNumbers=(array)=>{
+
+    let result=[];
+    array.forEach(element => {
+        
+    });
+}
+
+console.log(extractWordsAndNumbers(input));
